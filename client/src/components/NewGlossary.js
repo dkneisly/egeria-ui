52377134b6551349e2b2ca8e5f034714fc2d7d73
David Kneisly <dkneisly@gmail.com>
2020-06-25 10:45:26 -0700

    Added Final Form library

--- client/src/components/NewGlossary.js
+++ client/src/components/NewGlossary.js
@@ -2,63 +2,99 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { Form, FormGroup, TextInput, Button } from 'carbon-components-react'
+import { Form, Field } from 'react-final-form'
+import { FormGroup, TextInput, Button } from 'carbon-components-react'
 
 import { createGlossary } from '../actions/glossary'
 
-const NewGlossary = ({ dispatch }) => {
-  let glossaryName, glossaryDescription, qualifiedName;
-
-  return (
-    <div>
-      <Form
-        onSubmit={e => {
-          e.preventDefault()
-          if (!glossaryName.value.trim()) {
-            return
-          }
-          dispatch(createGlossary({
-            glossaryName: glossaryName.value
-          }))
-          glossaryName.value = ''
-        }}
-      >
-        <FormGroup legendText="New Glossary">
-          <TextInput id="glossaryName" helperText="Name of the new Glossary" labelText="Name" placeholder="Fancy Words" ref={node => (glossaryName = node)} />
-          <TextInput id="glossaryDescription" helperText="Description of the new Glossary" labelText="Description" placeholder="All the best words" ref={node => (glossaryDescription = node)} />
-          <TextInput id="qualifiedName" helperText="?" labelText="Qualified Name" placeholder="" ref={node => (qualifiedName = node)} />
-          <Button type="submit">Create Glossary</Button>
-        </FormGroup>
-      </Form>
-    </div>
-  )
+const onSubmit = values => {
+  alert(JSON.stringify(values))
 }
 
-export default connect()(NewGlossary)
+const validate = values => {
+  const errors = {}
+  if (!values["glossary-name"]) {
+    errors["glossary-name"] = 'Required'
+  }
+  return errors
+}
 
-// export default class NewGlossary extends Component {
+const NewGlossary = () => (
+  <Form
+    onSubmit={onSubmit}
+    validate={validate}
+    render={({ handleSubmit, form, invalid, submitting, pristine }) => (
+      <form onSubmit={handleSubmit}>
+        <h2>New Glossary</h2>
+        <Field name="glossary-name">
+          {({ input, meta }) => (
+            <div>
+              <TextInput id="glossary-name" {...input} labelText="Glossary Name" placeholder="Unique Name" />
+              {meta.error && meta.touched && <span>{meta.error}</span>}
+            </div>
+          )}
+        </Field>
+        <Field name="glossary-description">
+          {({ input, meta }) => (
+            <div>
+              <TextInput id="glossary-description" {...input} labelText="Description" placeholder="Description" />
+              {meta.error && meta.touched && <span>{meta.error}</span>}
+            </div>
+          )}
+        </Field>
+        <Field name="glossary-qualified-name">
+          {({ input, meta }) => (
+            <div>
+              <TextInput id="glossary-qualified-name" {...input} labelText="Qualified Name" placeholder="" />
+              {meta.error && meta.touched && <span>{meta.error}</span>}
+            </div>
+          )}
+        </Field>
+        <div className="buttons">
+          <Button type="submit" disabled={submitting || invalid}>
+            Submit
+          </Button>
+          <Button
+            type="button"
+            onClick={form.reset}
+            disabled={submitting || pristine}
+          >
+            Reset
+          </Button>
+        </div>
+      </form>
+    )}
+  />
+)
 
-//   render() {
-//     const { onSubmit } = this.props
+export default NewGlossary
 
-//     return (
-//       <span>
-//         <h1>Glossary Author</h1>
-//         <h3>
-//           <NavLink
-//             exact
-//             to={`/`}
-//           >
-//             Back to Glossary
-//           </NavLink>
-//         </h3>
-//       </span>
-//     )
-//   }
+
+//   let glossaryName, glossaryDescription, qualifiedName;
+
+//   return (
+//     <div>
+//       <Form
+//         onSubmit={e => {
+//           e.preventDefault()
+//           if (!glossaryName.value.trim()) {
+//             return
+//           }
+//           dispatch(createGlossary({
+//             glossaryName: glossaryName.value
+//           }))
+//           glossaryName.value = ''
+//         }}
+//       >
+//         <FormGroup legendText="New Glossary">
+//           <TextInput id="glossaryName" helperText="Name of the new Glossary" labelText="Name" placeholder="Fancy Words" ref={node => (glossaryName = node)} />
+//           <TextInput id="glossaryDescription" helperText="Description of the new Glossary" labelText="Description" placeholder="All the best words" ref={node => (glossaryDescription = node)} />
+//           <TextInput id="qualifiedName" helperText="?" labelText="Qualified Name" placeholder="" ref={node => (qualifiedName = node)} />
+//           <Button type="submit">Create Glossary</Button>
+//         </FormGroup>
+//       </Form>
+//     </div>
+//   )
 // }
 
-// NewGlossary.propTypes = {
-//   options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-//   value: PropTypes.string.isRequired,
-//   onChange: PropTypes.func.isRequired
-// }
+// export default connect()(NewGlossary)
